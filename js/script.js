e92ad278137e5c7f2195c4b3a7cfd50020e499a7
giuliogallo94 <giuliogallo1994@gmail.com>
2023-10-25 14:30:37 +0200

    add: array and WHILE cicle

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const myArray = [
+  "Biscotti",
+  "Latte",
+  "Pasta",
+  "Farina",
+  "Olio",
+  "Marmellata",
+  "Acqua",
+  "Mele",
+  "Nutella",
+  "Cornetti",
+];
+
+console.log(myArray);
+
+let i = 0;
+while (i < myArray.length) {
+  console.log(myArray[i]);
+  i++;
+}
